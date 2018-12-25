add956b9e8a7987bc827ad5bc7e06b443c643e04
sanyangkkun <hhpicket@asu.edu>
2018-12-25 13:14:12 -0700

    Title in place

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,20 @@ import { NavBar } from './components/navBar/navBar';
 class App extends Component {
   render() {
     return (
-      <div className="header-img" style={{ height: 1000 }}>
-        <div className="flex container justify-end">
-          <NavBar />
+      <>
+        <div className="header-img" style={{ height: 1000 }}>
+          <div className="flex container justify-end">
+            <NavBar />
+          </div>
         </div>
-      </div>
+        <div className="container flex justify-center">
+          <div className="text-center">
+            <h1>Colorado K9 Cuts</h1>
+            <h2>Professional, Experienced Mobile Dog Grooming</h2>
+            <h3>Cage Free, All-inclusive service at your front door!</h3>
+          </div>
+        </div>
+      </>
     );
   }
 }
