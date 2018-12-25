5ba1502e6d12e5f65257b87b5614e309c35845c9
sanyangkkun <hhpicket@asu.edu>
2018-12-25 11:50:44 -0700

    adding some files for tailwind + prettier

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return <div className="bg-blue-lighter h-48 w-48" />;
+  }
+}
+
+export default App;
