45d9fb9b6ebb17bbdf725b39e3939e2a9b10c403
sanyangkkun <hhpicket@asu.edu>
2018-12-26 20:14:13 -0700

    Purging unused CSS, made the image smaller

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 const withPlugins = require('next-compose-plugins');
-const withCSS = require('@zeit/next-css');
+const withCss = require('@zeit/next-css');
+const withPurgeCss = require('next-purgecss');
 const withTypescript = require('@zeit/next-typescript');
-module.exports = withPlugins([withTypescript, withCSS]);
+module.exports = withPlugins([withTypescript, withCss, withPurgeCss]);
+// module.exports = withPlugins([withTypescript, withCSS], {
+//   exportPathMap: async function(defaultPathMap) {
+//     return {
+//       '/index': { page: '/index' }
+//     };
+//   }
+// });
